8f59da6eef26f96925408772b4ab974577bbe90e
jeffandeko <j.andeko.j@gmail.com>
2018-08-17 06:34:59 -0700

    Feature, POST, GET method created in app/__init__.py
    Signed-off-by: jeffandeko <j.andeko.j@gmail.com>

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
+from os import abort
+
 from flask import request
 from flask.json import jsonify
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
-from models.py import Questions
 
 from instance.config import app_config
 
@@ -11,7 +12,7 @@ db = SQLAlchemy()
 
 
 def create_app(config_name, ):
-
+    from models.py import Questions
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
@@ -49,4 +50,36 @@ def create_app(config_name, ):
                 respond.status_code = 200
                 return respond
 
-    return app
+    @app.route('v1/questions/', methods=['GET', 'PUT', 'DELETE'])
+    def handle_questions():
+        question = Questions.query.filter().first
+        if not questions:
+            # 40$ HTTP exception to be raised
+            abort(404)
+
+        if request.method == 'DELETE':
+            question.delete()
+            return {"THe question was deleted successfully".format(questions.user_name)
+                    }, 200
+
+        elif request.method == 'PUT':
+            users_name = str(request.data.get('name', ''))
+            question.users_name = users_name
+            question.save()
+            respond = jsonify({
+                'users_name': question.users_name,
+                'date_created': question.date_created,
+                'date_modified': question.date_modified
+            })
+            respond.status_code = 200
+            return respond
+        else:
+            respond = jsonify({
+                'users_name': question.users_name,
+                'date_created': question.date_created,
+                'date_modified': question.date_modified
+            })
+            respond.status_code = 200
+            return respond
+
+        return app
